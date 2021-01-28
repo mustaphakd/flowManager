e3c2bd43b52d85a0d14fad20c3bf18d34b4eb583
musmus <musmus@musmuss-iMac.local>
2021-01-28 06:08:39 -0800

    MacOS support modified existing files

--- Analyzer.MacOS/Main.cs
+++ Analyzer.MacOS/Main.cs
@@ -7,6 +7,7 @@ namespace Analyzer.MacOS
         static void Main(string[] args)
         {
             NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
             NSApplication.Main(args);
         }
     }
